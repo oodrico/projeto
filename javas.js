c48d1857a22b76c8e5b7b70cd5b4cc47f7566921
ooodrico <drewsouza05@gmail.com>
2024-05-14 19:39:29 -0300

    projeto

---
+++ javas.js
@@ -0,0 +1,13 @@
+function clicar(){
+    var html = document.documentElement
+
+   html.classList.toggle('light')
+
+    //assim que faz com condiçao
+
+    // if(body.classList.constains('light')){
+    //  html.classList.add('light')
+    // }else{
+    //   html.classList.add('light')
+    //}
+}
